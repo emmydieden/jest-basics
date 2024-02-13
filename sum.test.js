7f5eec2fd0ea5883dae270c843f4d89c157da20a
Emmy Dieden <emmydieden@Emmys-MacBook-Pro.local>
2024-02-13 15:02:00 +0100

    second

---
+++ sum.test.js
@@ -0,0 +1,19 @@
+// test("two plus two is four", () => {
+//     expect(2 + 2).toBe(4)
+// })
+
+// test("object assignment", () => {
+//     const data = { one: 1}; 
+//     data["two"] = 2
+//     expect(data).toEqual({ one: 1, two: 2 }); 
+// }); 
+
+// test("null is falsy", () => {
+//     const n = null; 
+//     expect(n).toBeFalsy(); 
+// })
+
+test("one is truthy", () => {
+    const n = 1; 
+    expect(n).toBeTruthy()
+})
